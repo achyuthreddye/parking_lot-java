cdcf6a2b0c6b2b1eb7126149c914b744cc751156
achyuth Reddy <achyuth.reddy@everest.engineering>
2022-02-10 12:43:21 +0530

    feat: Implemented parking floor methods

--- src/main/java/ParkingFloor.java
+++ src/main/java/ParkingFloor.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +14,7 @@ public class ParkingFloor {
         this.totalNoOfSlots = totalNoOfSlots;
         this.noOfTrucks = noOfTrucks;
         this.noOfBikes = noOfBikes;
-        parkingFloor = new Slot[totalNoOfSlots];
+        this.parkingFloor = new Slot[totalNoOfSlots];
 
         for (int i = 0; i < noOfTrucks; i++) {
             this.parkingFloor[i] = new Slot(i+1, "truck");
@@ -27,16 +28,48 @@ public class ParkingFloor {
     }
 
     public Slot[] getParkingFloor() {
-        return parkingFloor;
+        return this.parkingFloor;
     }
     public Slot getParkingSlot(int slotId){
-        return parkingFloor[slotId];
+        return this.parkingFloor[slotId];
     }
+
+    public Map parkVehicleinParkingFloor(Vehicle vehicleToBeParked){
+        int nextNearestSlot = getNearestSlotInFloor(vehicleToBeParked.getVehicleType());
+        System.out.println("nextNearestSlot   " +nextNearestSlot );
+
+        Map parkingStatus = new HashMap();
+        parkingStatus.put("status", false);
+        parkingStatus.put("slotId", -1);
+
+        if( nextNearestSlot == -1){
+            System.out.println("No available parking slots");
+            return parkingStatus;
+        }
+        this.parkingFloor[nextNearestSlot - 1].allotSlot(vehicleToBeParked);
+        parkingStatus.put("status", true);
+        parkingStatus.put("slotId", nextNearestSlot);
+        return parkingStatus;
+    }
+
+    public  void unParkVehicleinParkingFloor(int slotId){
+
+        if(!this.parkingFloor[slotId - 1].isParked()){
+            System.out.println("No Vehicle is parked in here");
+            return;
+        }
+        this.parkingFloor[slotId - 1].unallotSlot();
+        return;
+    }
+
     public int getNearestSlotInFloor(String vehicleType){
         int freeSlotInFloor= -1;
-        for (int i = 0; i < this.totalNoOfSlots; i++) {
-            if((parkingFloor[i].getSlotType() == vehicleType) && !parkingFloor[i].isParked()){
-                freeSlotInFloor = parkingFloor[i].getSlotId();
+
+        System.out.println(parkingFloor.toString()  +  this.parkingFloor);
+
+        for (int i = 0; i < totalNoOfSlots; i++) {
+            if((this.parkingFloor[i].getSlotType().toLowerCase() == vehicleType.toLowerCase()) && !this.parkingFloor[i].isParked()){
+                freeSlotInFloor = this.parkingFloor[i].getSlotId();
                 break;
             }
         }
@@ -59,21 +92,77 @@ public class ParkingFloor {
         }
         return freeParkingSlotCount;
     }
-//    public Map<String, String[]> getAllFreeSlotsInFloor(){
-//        Map<String, String[]> freeParkingSlots=new HashMap();
-//
-//        for (int i = 0; i < this.totalNoOfSlots; i++) {
-//            if (parkingFloor[i].getSlotType() == "truck"){
-//                freeParkingSlots.put("truck", freeParkingSlots.getOrDefault("truck",0) + 1);
-//            }
-//            if (parkingFloor[i].getSlotType() == "bike"){
-//                freeParkingSlots.put("bike", freeParkingSlots.getOrDefault("bike",0) + 1);
-//            }
-//            if (parkingFloor[i].getSlotType() == "car"){
-//                freeParkingSlots.put("car", freeParkingSlots.getOrDefault("car",0) + 1);
-//            }
-//        }
-//        return freeParkingSlots;
-//    }
+
+    public Map<String, Integer> getAllOccupiedSlotCountInFloor(){
+        Map<String, Integer> occupiedParkingSlotCount=new HashMap();
+        occupiedParkingSlotCount.put("truck", 0);
+        occupiedParkingSlotCount.put("bike", 0);
+        occupiedParkingSlotCount.put("car", 0);
+
+        for (int i = 0; i < this.totalNoOfSlots; i++) {
+            if (parkingFloor[i].getSlotType() == "truck" && parkingFloor[i].isParked()){
+                occupiedParkingSlotCount.put("truck", occupiedParkingSlotCount.put("truck",occupiedParkingSlotCount.get("truck") + 1) );
+            }
+            if (parkingFloor[i].getSlotType() == "bike" && parkingFloor[i].isParked()){
+                occupiedParkingSlotCount.put("bike", occupiedParkingSlotCount.put("bike",occupiedParkingSlotCount.get("bike") + 1) );
+            }
+            if (parkingFloor[i].getSlotType() == "car" && parkingFloor[i].isParked()){
+                occupiedParkingSlotCount.put("car", occupiedParkingSlotCount.put("car",occupiedParkingSlotCount.get("car") + 1) );
+            }
+        }
+        return occupiedParkingSlotCount;
+    }
+
+    public Map<String, ArrayList<String>> getAllFreeSlotsInFloor(){
+        Map<String, ArrayList<String>> freeParkingSlots=new HashMap();
+        ArrayList<String> freeTruckSlots = new ArrayList<String> ();
+        ArrayList<String> freeBikeSlots = new ArrayList<String> ();
+        ArrayList<String> freeCarSlots = new ArrayList<String> ();
+
+        for (int i = 0; i < this.totalNoOfSlots; i++) {
+
+            if (parkingFloor[i].getSlotType() == "truck" && !parkingFloor[i].isParked()){
+                freeTruckSlots.add(String.valueOf(parkingFloor[i].getSlotId()));
+                freeParkingSlots.put("truck", freeTruckSlots);
+            }
+
+            if (parkingFloor[i].getSlotType() == "bike" && !parkingFloor[i].isParked()){
+                freeBikeSlots.add(String.valueOf(parkingFloor[i].getSlotId()));
+                freeParkingSlots.put("bike", freeBikeSlots);
+            }
+
+            if (parkingFloor[i].getSlotType() == "car" && !parkingFloor[i].isParked()){
+                freeCarSlots.add(String.valueOf(parkingFloor[i].getSlotId()));
+                freeParkingSlots.put("car", freeCarSlots);
+            }
+        }
+        return freeParkingSlots;
+    }
+
+    public Map<String, ArrayList<String>> getAllOccupiedSlotsInFloor(){
+        Map<String, ArrayList<String>> occupiedParkingSlots=new HashMap();
+        ArrayList<String> occupiedTruckSlots = new ArrayList<String> ();
+        ArrayList<String> occupiedBikeSlots = new ArrayList<String> ();
+        ArrayList<String> occupiedCarSlots = new ArrayList<String> ();
+
+        for (int i = 0; i < this.totalNoOfSlots; i++) {
+
+            if (parkingFloor[i].getSlotType() == "truck" && parkingFloor[i].isParked()){
+                occupiedTruckSlots.add(String.valueOf(parkingFloor[i].getSlotId()));
+                occupiedParkingSlots.put("truck", occupiedTruckSlots);
+            }
+
+            if (parkingFloor[i].getSlotType() == "bike" && parkingFloor[i].isParked()){
+                occupiedBikeSlots.add(String.valueOf(parkingFloor[i].getSlotId()));
+                occupiedParkingSlots.put("bike", occupiedBikeSlots);
+            }
+
+            if (parkingFloor[i].getSlotType() == "car" && parkingFloor[i].isParked()){
+                occupiedCarSlots.add(String.valueOf(parkingFloor[i].getSlotId()));
+                occupiedParkingSlots.put("car", occupiedCarSlots);
+            }
+        }
+        return occupiedParkingSlots;
+    }
 
 }
--- src/test/java/ParkingFloorTest.java
+++ src/test/java/ParkingFloorTest.java
@@ -1,13 +1,24 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingFloorTest {
     ParkingFloor parkingFloor = new ParkingFloor(6,2,2);
+    Vehicle truck1 = new Vehicle("Ka40m8500", "red", "truck");
+    Vehicle truck2 = new Vehicle("Ka40m8501", "red", "truck");
+    Vehicle truck3 = new Vehicle("Ka40m8503", "red", "truck");
+
+    Vehicle bike1 = new Vehicle("Ka40m8500", "red", "bike");
+    Vehicle bike2 = new Vehicle("Ka40m8501", "red", "bike");
+    Vehicle bike3 = new Vehicle("Ka40m8503", "red", "bike");
+//    Vehicle truck3 = new Vehicle("Ka40m8501", "red", "bike");
+    Vehicle truck4 = new Vehicle("Ka40m8501", "red", "car");
 
     @Test
     public void testingForNextTruckSlot(){
@@ -31,7 +42,6 @@ class ParkingFloorTest {
 
     @Test
     public void testingGetAllFreeSlotCountInFloor(){
-//        Assertions.assertEquals(-1,parkingFloor.getNearestSlotInFloor("any Invalid data"));
         Map<String, Integer> freeParkingSlotCount=new HashMap();
         freeParkingSlotCount = parkingFloor.getAllFreeSlotCountInFloor();
 
@@ -40,7 +50,54 @@ class ParkingFloorTest {
         assertTrue(freeParkingSlotCount.containsKey("car") && freeParkingSlotCount.get("car") == 2);
     }
 
+    @Test
+    public void testingGetAllOccupiedSlotCountInFloor(){
+        Map<String, Integer> occupiedParkingSlotCount=new HashMap();
+        occupiedParkingSlotCount = parkingFloor.getAllOccupiedSlotCountInFloor();
 
+        assertTrue(occupiedParkingSlotCount.containsKey("truck") && occupiedParkingSlotCount.get("truck") == 0);
+        assertTrue(occupiedParkingSlotCount.containsKey("bike") && occupiedParkingSlotCount.get("bike") == 0);
+        assertTrue(occupiedParkingSlotCount.containsKey("car") && occupiedParkingSlotCount.get("car") == 0);
+    }
+
+    @Test
+    public  void testingForParkingTruck1(){
+//        assertTrue(Boolean.parseBoolean(parkingFloor.parkVehicleinParkingFloor(truck1).get("status").toString()));
+        assertEquals(1, parkingFloor.parkVehicleinParkingFloor(truck1).get("slotId"));
+        assertEquals(2, parkingFloor.parkVehicleinParkingFloor(truck2).get("slotId"));
+        assertFalse(Boolean.parseBoolean(parkingFloor.parkVehicleinParkingFloor(truck3).get("status").toString()));
+    }
+
+    @Test
+    public  void testingForUnParkingTruck1(){
+        assertEquals(1, parkingFloor.parkVehicleinParkingFloor(truck1).get("slotId"));
+        assertEquals(2, parkingFloor.parkVehicleinParkingFloor(truck2).get("slotId"));
+        parkingFloor.unParkVehicleinParkingFloor(1);
+        int nearestParkingSlotForTruck = parkingFloor.getNearestSlotInFloor("truck");
+        assertEquals(1, nearestParkingSlotForTruck);
+    }
+
+
+
+    @Test
+    public  void testingForParkingTruck3(){
+        assertTrue(Boolean.parseBoolean(parkingFloor.parkVehicleinParkingFloor(truck3).get("status").toString()));
+    }
+
+//    @Test
+//    public  void testingForParkingBike1(){
+//        assertTrue(Boolean.parseBoolean(parkingFloor.parkVehicleinParkingFloor(bike1).get("status").toString()));
+//    }
+//
+//    @Test
+//    public  void testingForParkingBike2(){
+//        assertTrue(Boolean.parseBoolean(parkingFloor.parkVehicleinParkingFloor(bike2).get("status").toString()));
+//    }
+//
+//    @Test
+//    public  void testingForParkingBike3(){
+//        assertTrue(Boolean.parseBoolean(parkingFloor.parkVehicleinParkingFloor(bike3).get("status").toString()));
+//    }
 
 
 
