554e889e3a6edc7fd9c01c52d3480e5a67abe902
achyuth Reddy <achyuth.reddy@everest.engineering>
2022-02-10 06:38:04 +0530

    feat: Implemented Vehicle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,23 @@
 public class Vehicle {
+    private String vehicleNumber;
+    private  String vehicleColor;
+    private String vehicleType;
+
+    public Vehicle(String vehicleNumber,String vehicleColor,String vehicleType){
+        this.vehicleNumber = vehicleNumber;
+        this.vehicleColor = vehicleColor;
+        this.vehicleType = vehicleType;
+    }
+
+    public String getVehicleColor() {
+        return vehicleColor;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
 }
