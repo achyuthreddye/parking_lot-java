83fbafcc9b02cee1bd363f243cdfbbcbb2f6d47a
achyuth Reddy <achyuth.reddy@everest.engineering>
2022-02-10 06:52:10 +0530

    feat: Implemented Slot class

---
+++ src/main/java/Slot.java
@@ -0,0 +1,33 @@
+public class Slot {
+    private Vehicle vehicle;
+    private boolean isParked = false;
+    private int slotId;
+    private String slotType;
+
+    public Slot(int slotId, String slotType) {
+        this.slotId = slotId;
+        this.slotType = slotType;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public String getSlotType() {
+        return slotType;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void allotSlot(Vehicle vehicleTobeParked){
+        this.isParked = true;
+        this.vehicle = vehicleTobeParked;
+    }
+
+    public  void unallotSlot(){
+        this.isParked = false;
+        this.vehicle = null;
+    }
+}
